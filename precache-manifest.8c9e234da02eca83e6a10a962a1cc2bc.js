bd7701779e1ab4d0b385bdf08a2f86c80181ef2d
Alejandro Bustos Nuñez <alejandro@Alejandros-MacBook-Pro.local>
2020-01-29 23:22:37 -0300

    Updates

---
+++ precache-manifest.8c9e234da02eca83e6a10a962a1cc2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a264235670012d1ba1b2d1aa460fc0",
+    "url": "/websynth/index.html"
+  },
+  {
+    "revision": "d1ee3aada26d63317cb3",
+    "url": "/websynth/static/css/main.7498e068.chunk.css"
+  },
+  {
+    "revision": "c416fef89c592e8576f4",
+    "url": "/websynth/static/js/2.e2c21136.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/websynth/static/js/2.e2c21136.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1ee3aada26d63317cb3",
+    "url": "/websynth/static/js/main.2e79c117.chunk.js"
+  },
+  {
+    "revision": "43a47033287d3dccbdde",
+    "url": "/websynth/static/js/runtime-main.aa64afbd.js"
+  }
+]);
